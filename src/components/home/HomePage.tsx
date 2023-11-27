64e5f101105a62b26894c37ba00ed3dfa9e3c210
alx <aleksandar.zedi@gmail.com>
2023-11-27 01:34:54 +0100

    Minor fix to responsive on Splashscreen

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -18,17 +18,19 @@ function HomePage() {
       <div className="absolute top-6 left-0">
         <LanguageSwitcher />
       </div>
-      <div className="w-full sm:w-1/2 flex justify-center mb-20 lg:mb-0">
-        <WithLink href="/">
-          <LogoComponent size={width > 640 ? LOGO_SIZE_LG : LOGO_SIZE_MD} />
-        </WithLink>
-      </div>
-      <div className="w-full sm:w-1/2 flex flex-col items-center">
-        <div className="px-4 relative bg-white md:px-0 w-full max-w-[420px] sm:min-w-[420px]">
-          <ModalContextProvider>
-            <Login />
-            <Signup />
-          </ModalContextProvider>
+      <div className="w-full max-w-[1280px] h-full flex flex-col justify-center items-center lg:flex-row lg:items-center">
+        <div className="w-full sm:w-1/2 flex justify-center mb-20 lg:mb-0">
+          <WithLink href="/">
+            <LogoComponent size={width > 640 ? LOGO_SIZE_LG : LOGO_SIZE_MD} />
+          </WithLink>
+        </div>
+        <div className="w-full sm:w-1/2 flex flex-col items-center">
+          <div className="px-4 relative bg-white md:px-0 w-full max-w-[420px] sm:min-w-[420px]">
+            <ModalContextProvider>
+              <Login />
+              <Signup />
+            </ModalContextProvider>
+          </div>
         </div>
       </div>
     </div>
